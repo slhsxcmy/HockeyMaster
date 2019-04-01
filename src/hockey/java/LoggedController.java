a7d313cadb53fd33aeb8909166e9cbfd8169216b
Mingyu Cui <mingyucu@usc.edu>
2019-03-31 17:52:25 -0700

    Stats controller baseline

--- src/hockey/java/LoggedController.java
+++ src/hockey/java/LoggedController.java
@@ -30,23 +30,11 @@ public class LoggedController {
 	
 	public void join(ActionEvent event) throws IOException{
 		
-		Parent root = FXMLLoader.load(getClass().getResource("/hockey/fxml/Login.fxml"));
-		Scene scene = new Scene(root);
-		scene.getStylesheets().add(getClass().getResource("/hockey/css/Login.css").toExternalForm());
-		
-		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-		window.setScene(scene);
-		window.show();
+	
 	}
 	
 	public void goStats(ActionEvent event) throws IOException{
-		Parent root = FXMLLoader.load(getClass().getResource("/hockey/fxml/Register.fxml"));
-		Scene scene = new Scene(root);
-		scene.getStylesheets().add(getClass().getResource("/hockey/css/Register.css").toExternalForm());
-		
-		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-		window.setScene(scene);
-		window.show();	
+
 	}
 	
 	public void signout(ActionEvent event) throws IOException{
