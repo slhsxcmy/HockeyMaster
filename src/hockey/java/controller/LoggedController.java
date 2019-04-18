19e0babc023d77639f8a09be7b6428f414c3c14e
Mingyu Cui <mingyucu@usc.edu>
2019-04-17 21:38:14 -0700

    Update LoggedController.java

--- src/hockey/java/controller/LoggedController.java
+++ src/hockey/java/controller/LoggedController.java
@@ -50,16 +50,10 @@ public class LoggedController {
 
 	}
 	
-	public void signout(ActionEvent event) throws IOException{
-		// add sign out logic
-		
-		Parent root = FXMLLoader.load(getClass().getResource("/hockey/fxml/Menu.fxml"));
-		Scene scene = new Scene(root);
-		scene.getStylesheets().add(getClass().getResource("/hockey/css/Menu.css").toExternalForm());
-		
-		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-		window.setScene(scene);
-		window.show();	
+	public void signout(ActionEvent event) throws IOException{	
+		PacketAttempt p = new PacketAttempt(Constants.SIGNOUTATTEMPT, Hockey.getUser().getId());		
+		Hockey.getNetwork().getClient().sendTCP(p);
+		System.out.println("sent signout attempt");
 	}
 
 	public void setMessage(String string) {
